9ced50765e9d20a4086d6b31e1f87e1428ccac4f
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-03-11 22:49:34 +0800

    加入專案檔案。

---
+++ TestingExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestingExample
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+            Console.WriteLine($"來源序列為：{string.Join(", ", list)}");
+
+            Console.WriteLine("請輸入要判斷的數字：");
+            int value;
+            int.TryParse(Console.ReadLine(), out value);
+
+            Console.WriteLine($"來源序列是否包含指定的項目：");
+            Console.WriteLine(Contains(list, value));
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 判斷來源序列是否包含指定的項目，使用預設相等比較子。
+        /// </summary>
+        /// <typeparam name="TSource">來源序列型別。</typeparam>
+        /// <param name="source">來源序列。</param>
+        /// <param name="value">指定的項目。</param>
+        /// <returns>
+        /// 如果來源序列包含具有指定的值為 <c>true</c>，否則為 <c>false</c>。
+        /// </returns>
+        /// <exception cref="ArgumentNullException">source</exception>
+        public static bool Contains<TSource>(IEnumerable<TSource> source, TSource value)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            var comparer = EqualityComparer<TSource>.Default;
+
+            foreach (var item in source)
+            {
+                if (comparer.Equals(item, value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ TestingExampleTests/ProgramTests.cs
@@ -0,0 +1,108 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace TestingExample.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        [TestCategory("Program")]
+        [TestProperty("Program", "Contains")]
+        public void Contains_當傳入引數source為Null時_應拋出ArgumentNullException的例外狀況()
+        {
+            // Arrange
+            IEnumerable<int> source = null;
+            var value = 1;
+            var message = $"值不能為 null。{Environment.NewLine}參數名稱: source";
+
+            // Act
+            Action action = () => Program.Contains(source, value);
+
+            // Assert
+            action.ShouldThrow<ArgumentNullException>().And.Message.Should().Be(message);
+        }
+
+        [TestMethod()]
+        [TestCategory("Program")]
+        [TestProperty("Program", "Contains")]
+        public void Contains_當傳入引數source為空集合時_應回傳False()
+        {
+            // Arrange
+            var source = new List<int>();
+            var value = 1;
+
+            // Act
+            var actual = Program.Contains(source, value);
+
+            // Assert
+            actual.Should().BeFalse();
+        }
+
+        [TestMethod()]
+        [TestCategory("Program")]
+        [TestProperty("Program", "Contains")]
+        public void Contains_當傳入引數source為只有單元素集合與value為集合內沒有的項目時_應回傳False()
+        {
+            // Arrange
+            var source = new List<int> { 1 };
+            var value = 0;
+
+            // Act
+            var actual = Program.Contains(source, value);
+
+            // Assert
+            actual.Should().BeFalse();
+        }
+
+        [TestMethod()]
+        [TestCategory("Program")]
+        [TestProperty("Program", "Contains")]
+        public void Contains_當傳入引數source為多元素集合與value為集合內沒有的項目時_應回傳False()
+        {
+            // Arrange
+            var source = new List<int> { 1, 2, 3 };
+            var value = 0;
+
+            // Act
+            var actual = Program.Contains(source, value);
+
+            // Assert
+            actual.Should().BeFalse();
+        }
+
+        [TestMethod()]
+        [TestCategory("Program")]
+        [TestProperty("Program", "Contains")]
+        public void Contains_當傳入引數source為只有單元素集合與value為集合內有的項目時_應回傳True()
+        {
+            // Arrange
+            var source = new List<int> { 1 };
+            var value = 1;
+
+            // Act
+            var actual = Program.Contains(source, value);
+
+            // Assert
+            actual.Should().BeTrue();
+        }
+
+        [TestMethod()]
+        [TestCategory("Program")]
+        [TestProperty("Program", "Contains")]
+        public void Contains_當傳入引數source為多元素集合與value為集合內有的項目時_應回傳True()
+        {
+            // Arrange
+            var source = new List<int> { 1, 2, 3 };
+            var value = 2;
+
+            // Act
+            var actual = Program.Contains(source, value);
+
+            // Assert
+            actual.Should().BeTrue();
+        }
+    }
+}
