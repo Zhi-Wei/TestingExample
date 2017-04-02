4985ca6065e65538efc1c6ff5993db87c1162a17
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-04-02 15:31:20 +0800

    1. 移除 Program.Contains 方法。 2. 在 Program.Main 方法中改用 TestExample.Contains 方法。

--- TestingExample/Program.cs
+++ TestingExample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using TestingExample.Implements;
 
 namespace TestingExample
 {
@@ -15,38 +16,10 @@ namespace TestingExample
             int.TryParse(Console.ReadLine(), out value);
 
             Console.WriteLine($"來源序列是否包含指定的項目：");
-            Console.WriteLine(Contains(list, value));
+            var testExample = new TestExample();
+            Console.WriteLine(testExample.Contains(list, value));
 
             Console.ReadLine();
-        }
-
-        /// <summary>
-        /// 判斷來源序列是否包含指定的項目，使用預設相等比較子。
-        /// </summary>
-        /// <typeparam name="TSource">來源序列型別。</typeparam>
-        /// <param name="source">來源序列。</param>
-        /// <param name="value">指定的項目。</param>
-        /// <returns>
-        /// 如果來源序列包含具有指定的值為 <c>true</c>，否則為 <c>false</c>。
-        /// </returns>
-        /// <exception cref="ArgumentNullException">source</exception>
-        public static bool Contains<TSource>(IEnumerable<TSource> source, TSource value)
-        {
-            if (source == null)
-            {
-                throw new ArgumentNullException(nameof(source));
-            }
-
-            var comparer = EqualityComparer<TSource>.Default;
-
-            foreach (var item in source)
-            {
-                if (comparer.Equals(item, value))
-                {
-                    return true;
-                }
-            }
-            return false;
         }
     }
 }
