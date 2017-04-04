b5a4fdfa52bdf82fe26345243159d45b7a188b38
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-04-04 17:11:03 +0800

    1. 移除 ProgramTests 測試類別。 2. 新建 TestExampleTests 測試類別。

--- TestingExampleTests/Implements/TestExampleTests.cs
+++ TestingExampleTests/Implements/TestExampleTests.cs
@@ -3,14 +3,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 
-namespace TestingExample.Tests
+namespace TestingExample.Implements.Tests
 {
     [TestClass()]
-    public class ProgramTests
+    public class TestExampleTests
     {
+        #region -- 前置準備 --
+
+        private TestExample GetSystemUnderTestInstance()
+        {
+            return new TestExample();
+        }
+
+        #endregion -- 前置準備 --
+
         [TestMethod()]
-        [TestCategory("Program")]
-        [TestProperty("Program", "Contains")]
+        [TestCategory("TestExample")]
+        [TestProperty("TestExample", "Contains")]
         [TestCategory("Edge Coverage")]
         [TestProperty("Edge Coverage", "1")]
         [TestCategory("Test Path Coverage")]
@@ -21,17 +30,18 @@ namespace TestingExample.Tests
             IEnumerable<int> source = null;
             var value = 1;
             var message = $"值不能為 null。{Environment.NewLine}參數名稱: source";
+            var sut = this.GetSystemUnderTestInstance();
 
             // Act
-            Action action = () => Program.Contains(source, value);
+            Action action = () => sut.Contains(source, value);
 
             // Assert
             action.ShouldThrow<ArgumentNullException>().And.Message.Should().Be(message);
         }
 
         [TestMethod()]
-        [TestCategory("Program")]
-        [TestProperty("Program", "Contains")]
+        [TestCategory("TestExample")]
+        [TestProperty("TestExample", "Contains")]
         [TestCategory("Edge-Pair Coverage")]
         [TestProperty("Edge-Pair Coverage", "1")]
         [TestCategory("Test Path Coverage")]
@@ -41,17 +51,18 @@ namespace TestingExample.Tests
             // Arrange
             var source = new List<int>();
             var value = 1;
+            var sut = this.GetSystemUnderTestInstance();
 
             // Act
-            var actual = Program.Contains(source, value);
+            var actual = sut.Contains(source, value);
 
             // Assert
             actual.Should().BeFalse();
         }
 
         [TestMethod()]
-        [TestCategory("Program")]
-        [TestProperty("Program", "Contains")]
+        [TestCategory("TestExample")]
+        [TestProperty("TestExample", "Contains")]
         [TestCategory("Test Path Coverage")]
         [TestProperty("Test Path Coverage", "3")]
         public void Contains_當傳入引數source為只有單元素集合與value為集合內沒有的項目時_應回傳False()
@@ -59,17 +70,18 @@ namespace TestingExample.Tests
             // Arrange
             var source = new List<int> { 1 };
             var value = 0;
+            var sut = this.GetSystemUnderTestInstance();
 
             // Act
-            var actual = Program.Contains(source, value);
+            var actual = sut.Contains(source, value);
 
             // Assert
             actual.Should().BeFalse();
         }
 
         [TestMethod()]
-        [TestCategory("Program")]
-        [TestProperty("Program", "Contains")]
+        [TestCategory("TestExample")]
+        [TestProperty("TestExample", "Contains")]
         [TestCategory("Edge Coverage")]
         [TestProperty("Edge Coverage", "2")]
         [TestCategory("Edge-Pair Coverage")]
@@ -79,17 +91,18 @@ namespace TestingExample.Tests
             // Arrange
             var source = new List<int> { 1, 2, 3 };
             var value = 0;
+            var sut = this.GetSystemUnderTestInstance();
 
             // Act
-            var actual = Program.Contains(source, value);
+            var actual = sut.Contains(source, value);
 
             // Assert
             actual.Should().BeFalse();
         }
 
         [TestMethod()]
-        [TestCategory("Program")]
-        [TestProperty("Program", "Contains")]
+        [TestCategory("TestExample")]
+        [TestProperty("TestExample", "Contains")]
         [TestCategory("Test Path Coverage")]
         [TestProperty("Test Path Coverage", "4")]
         public void Contains_當傳入引數source為只有單元素集合與value為集合內有的項目時_應回傳True()
@@ -97,17 +110,18 @@ namespace TestingExample.Tests
             // Arrange
             var source = new List<int> { 1 };
             var value = 1;
+            var sut = this.GetSystemUnderTestInstance();
 
             // Act
-            var actual = Program.Contains(source, value);
+            var actual = sut.Contains(source, value);
 
             // Assert
             actual.Should().BeTrue();
         }
 
         [TestMethod()]
-        [TestCategory("Program")]
-        [TestProperty("Program", "Contains")]
+        [TestCategory("TestExample")]
+        [TestProperty("TestExample", "Contains")]
         [TestCategory("Edge Coverage")]
         [TestProperty("Edge Coverage", "3")]
         [TestCategory("Edge-Pair Coverage")]
@@ -119,9 +133,10 @@ namespace TestingExample.Tests
             // Arrange
             var source = new List<int> { 1, 2, 3 };
             var value = 2;
+            var sut = this.GetSystemUnderTestInstance();
 
             // Act
-            var actual = Program.Contains(source, value);
+            var actual = sut.Contains(source, value);
 
             // Assert
             actual.Should().BeTrue();
