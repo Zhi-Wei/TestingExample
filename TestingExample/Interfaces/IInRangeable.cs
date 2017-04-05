93a27a700981ae3243444ed2f260005ae3758991
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-04-05 21:58:59 +0800

    新建 IInRangeable 定義在特定的範圍內的方法介面。

---
+++ TestingExample/Interfaces/IInRangeable.cs
@@ -0,0 +1,17 @@
+﻿namespace TestingExample.Interfaces
+{
+    /// <summary>
+    /// 定義在特定的範圍內的方法。
+    /// </summary>
+    public interface IInRangeable
+    {
+        /// <summary>
+        /// 判斷指定的數字是否在特定的範圍內。
+        /// </summary>
+        /// <param name="number">指定的數字。</param>
+        /// <returns>
+        /// 若指定的數字在特定的範圍內則回傳 <c>true</c>，否則回傳 <c>false</c>。
+        /// </returns>
+        bool IsInRange(int number);
+    }
+}
