9c69c279aff36748a872be2082c8917acac4c4ee
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-04-12 19:21:47 +0800

    新建 IPasswordValidator 定義密碼驗證方法介面。

---
+++ TestingExample/Interfaces/IPasswordValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace TestingExample.Interfaces
+{
+    /// <summary>
+    /// 定義密碼驗證方法。
+    /// </summary>
+    public interface IPasswordValidator
+    {
+        /// <summary>
+        /// 驗證密碼是否符合指定的規則。
+        /// </summary>
+        /// <param name="password">要被驗證的密碼。</param>
+        /// <returns>符合指定的規則為 <c>true</c>，否則為 <c>false</c>。</returns>
+        bool Validate(string password);
+    }
+}
