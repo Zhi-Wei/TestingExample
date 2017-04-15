baa6927900034cc8dab0e74fee56e09b91c575e2
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-04-15 14:25:27 +0800

    1. 新建 PasswordValidatorTests 測試類別。 2. 新增 PasswordValidator.Validate 方法的 Equivalence Partition 測試案例。

---
+++ TestingExampleTests/Implements/PasswordValidatorTests.cs
@@ -0,0 +1,200 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestingExample.Implements.Tests
+{
+    [TestClass()]
+    public class PasswordValidatorTests
+    {
+        #region -- 前置準備 --
+
+        private PasswordValidator GetSystemUnderTestInstance()
+        {
+            return new PasswordValidator();
+        }
+
+        #endregion -- 前置準備 --
+
+        [TestMethod()]
+        [TestCategory("PasswordValidator")]
+        [TestProperty("PasswordValidator", "Validate")]
+        [TestCategory("Equivalence Partition")]
+        [TestProperty("Equivalence Partition", "1")]
+        public void Validate_當傳入引數password為Null時_應拋出ArgumentNullException的例外狀況()
+        {
+            // Arrange
+            string password = null;
+            var message = $"值不能為 null。{Environment.NewLine}參數名稱: password";
+            var sut = this.GetSystemUnderTestInstance();
+
+            // Act
+            Action action = () => sut.Validate(password);
+
+            // Assert
+            action.ShouldThrow<ArgumentNullException>().And.Message.Should().Be(message);
+        }
+
+        [TestMethod()]
+        [TestCategory("PasswordValidator")]
+        [TestProperty("PasswordValidator", "Validate")]
+        [TestCategory("Equivalence Partition")]
+        [TestProperty("Equivalence Partition", "2")]
+        public void Validate_當傳入引數password為空字串時_應回傳False()
+        {
+            // Arrange
+            string password = string.Empty;
+            var sut = this.GetSystemUnderTestInstance();
+
+            // Act
+            var actual = sut.Validate(password);
+
+            // Assert
+            actual.Should().BeFalse();
+        }
+
+        [TestMethod()]
+        [TestCategory("PasswordValidator")]
+        [TestProperty("PasswordValidator", "Validate")]
+        [TestCategory("Equivalence Partition")]
+        [TestProperty("Equivalence Partition", "3")]
+        public void Validate_當傳入引數password為空格字串時_應回傳False()
+        {
+            // Arrange
+            string password = " ";
+            var sut = this.GetSystemUnderTestInstance();
+
+            // Act
+            var actual = sut.Validate(password);
+
+            // Assert
+            actual.Should().BeFalse();
+        }
+
+        [TestMethod()]
+        [TestCategory("PasswordValidator")]
+        [TestProperty("PasswordValidator", "Validate")]
+        [TestCategory("Equivalence Partition")]
+        [TestProperty("Equivalence Partition", "4")]
+        public void Validate_當傳入引數password為長度小於8的字串時_應回傳False()
+        {
+            // Arrange
+            string password = "0Ab@";
+            var sut = this.GetSystemUnderTestInstance();
+
+            // Act
+            var actual = sut.Validate(password);
+
+            // Assert
+            actual.Should().BeFalse();
+        }
+
+        [TestMethod()]
+        [TestCategory("PasswordValidator")]
+        [TestProperty("PasswordValidator", "Validate")]
+        [TestCategory("Equivalence Partition")]
+        [TestProperty("Equivalence Partition", "5")]
+        public void Validate_當傳入引數password為長度大於32的字串時_應回傳False()
+        {
+            // Arrange
+            string password = "0Ab@0Ab@0Ab@0Ab@0Ab@0Ab@0Ab@0Ab@0Ab@";
+            var sut = this.GetSystemUnderTestInstance();
+
+            // Act
+            var actual = sut.Validate(password);
+
+            // Assert
+            actual.Should().BeFalse();
+        }
+
+        [TestMethod()]
+        [TestCategory("PasswordValidator")]
+        [TestProperty("PasswordValidator", "Validate")]
+        [TestCategory("Equivalence Partition")]
+        [TestProperty("Equivalence Partition", "6")]
+        public void Validate_當傳入引數password為長度大於等於8且小於等於32且包含大小寫字母與數字與符號的字串時_應回傳True()
+        {
+            // Arrange
+            string password = "0Ab@0Ab@";
+            var sut = this.GetSystemUnderTestInstance();
+
+            // Act
+            var actual = sut.Validate(password);
+
+            // Assert
+            actual.Should().BeTrue();
+        }
+
+        [TestMethod()]
+        [TestCategory("PasswordValidator")]
+        [TestProperty("PasswordValidator", "Validate")]
+        [TestCategory("Equivalence Partition")]
+        [TestProperty("Equivalence Partition", "7")]
+        public void Validate_當傳入引數password為長度等於8且無符號的字串時_應回傳False()
+        {
+            // Arrange
+            string password = "0Abc0Abc";
+            var sut = this.GetSystemUnderTestInstance();
+
+            // Act
+            var actual = sut.Validate(password);
+
+            // Assert
+            actual.Should().BeFalse();
+        }
+
+        [TestMethod()]
+        [TestCategory("PasswordValidator")]
+        [TestProperty("PasswordValidator", "Validate")]
+        [TestCategory("Equivalence Partition")]
+        [TestProperty("Equivalence Partition", "8")]
+        public void Validate_當傳入引數password為長度等於8且無數字的字串時_應回傳False()
+        {
+            // Arrange
+            string password = "zAb@zAb@";
+            var sut = this.GetSystemUnderTestInstance();
+
+            // Act
+            var actual = sut.Validate(password);
+
+            // Assert
+            actual.Should().BeFalse();
+        }
+
+        [TestMethod()]
+        [TestCategory("PasswordValidator")]
+        [TestProperty("PasswordValidator", "Validate")]
+        [TestCategory("Equivalence Partition")]
+        [TestProperty("Equivalence Partition", "9")]
+        public void Validate_當傳入引數password為長度等於8且無小寫字母的字串時_應回傳False()
+        {
+            // Arrange
+            string password = "0AB@0AB@";
+            var sut = this.GetSystemUnderTestInstance();
+
+            // Act
+            var actual = sut.Validate(password);
+
+            // Assert
+            actual.Should().BeFalse();
+        }
+
+        [TestMethod()]
+        [TestCategory("PasswordValidator")]
+        [TestProperty("PasswordValidator", "Validate")]
+        [TestCategory("Equivalence Partition")]
+        [TestProperty("Equivalence Partition", "10")]
+        public void Validate_當傳入引數password為長度等於8且無大寫字母的字串時_應回傳False()
+        {
+            // Arrange
+            string password = "0ab@0ab@";
+            var sut = this.GetSystemUnderTestInstance();
+
+            // Act
+            var actual = sut.Validate(password);
+
+            // Assert
+            actual.Should().BeFalse();
+        }
+    }
+}
